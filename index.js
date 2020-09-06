d8ee1045550c11f516fb4412d1b86a3db3ead96e
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-06 05:28:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,10 @@ function scuberGreetingForFeet(){
  let greeting 
  if(distance =< 400) {
    greeting = 'This one is on me!';
- else if (distance > )
+ }
+ else if (distance >2000) {
+   greeting = 'I will gladly take your thirty bucks.'
+}
  }
 }
 
