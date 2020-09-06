02cf9f44423227247ca131f07f45718f9e0334e1
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-09-06 06:16:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function ternaryCheckCity(location) {
 
 function switchOnCharmFromTip(tip){
  switch (tip) {
- case 'generous' :
+  case 'generous' :
    return 'Thank you so much.'
  break;
- case 'not as generous' :
+  case 'not as generous' :
    return 'Thank you.'
    break;
  default :
